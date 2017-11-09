aed547bd96a8a3e579b6027475cf07fb58a8e6ac
Loures <Loures@github.com>
2017-11-09 22:08:03 +0100

    Rimossi stringify che creavano casino con l'output (quote escape e altre cose brutte)

--- index.js
+++ index.js
@@ -18,21 +18,21 @@ function v1Endpoints(server, rooms, lessons, degrees, courses) {
     server.get('/v1/degrees', (req, res, next) => {
         //res.json(degrees)
 		res.header('content-type', config.debug ? 'text' : 'json')
-		res.send(config.debug ? prettyjson.render(degrees) : JSON.stringify(degrees))
+		res.send(config.debug ? prettyjson.render(degrees) : degrees)
         next()
     })
 
     server.get('/v1/degrees/:abbr/courses', (req, res, next) => {
         //res.json(courses.filter(x => x.laurea == req.params.abbr))
 		res.header('content-type', config.debug ? 'text' : 'json')
-		res.send(config.debug ? prettyjson.render(courses.filter(x => x.laurea == req.params.abbr)) : JSON.stringify(courses.filter(x => x.laurea == req.params.abbr)))
+		res.send(config.debug ? prettyjson.render(courses.filter(x => x.laurea == req.params.abbr)) : courses.filter(x => x.laurea == req.params.abbr))
         next()
     })
 
     server.get('/v1/degrees/:abbr/courses/semesters/:sem', (req, res, next) => {
         //res.json(courses.filter(x => x.laurea == req.params.abbr && x.semestre == req.params.sem))
 		res.header('content-type', config.debug ? 'text' : 'json')
-		res.send(config.debug ? prettyjson.render(courses.filter(x => x.laurea == req.params.abbr && x.semestre == req.params.sem)) : JSON.stringify(courses.filter(x => x.laurea == req.params.abbr && x.semestre == req.params.sem)))
+		res.send(config.debug ? prettyjson.render(courses.filter(x => x.laurea == req.params.abbr && x.semestre == req.params.sem)) : courses.filter(x => x.laurea == req.params.abbr && x.semestre == req.params.sem))
         next()
     })
 
@@ -44,14 +44,14 @@ function v1Endpoints(server, rooms, lessons, degrees, courses) {
         }
         //res.json(Array.from(buildings))
         res.header('content-type', config.debug ? 'text' : 'json')
-        config.debug ? res.send(prettyjson.render(Array.from(buildings))) : res.send(JSON.stringify(Array.from(buildings)))
+        config.debug ? res.send(prettyjson.render(Array.from(buildings))) : res.send(Array.from(buildings))
         next()
     })
 
     server.get('/v1/buildings/:name/rooms', (req, res, next) => {
         //res.json(queries.getRoomsInBuilding(rooms, req.params.name))
 		res.header('content-type', config.debug ? 'text' : 'json')
-		res.send(config.debug ? prettyjson.render(queries.getRoomsInBuilding(rooms, req.params.name)) : JSON.stringify(queries.getRoomsInBuilding(rooms, req.params.name)))
+		res.send(config.debug ? prettyjson.render(queries.getRoomsInBuilding(rooms, req.params.name)) : queries.getRoomsInBuilding(rooms, req.params.name))
         next()
     })
 
@@ -59,7 +59,7 @@ function v1Endpoints(server, rooms, lessons, degrees, courses) {
     server.get('/v1/lessons/days/:day', (req, res, next) => {
         //res.json(lessons.filter(x => x.lessons == req.params.day))
 		res.header('content-type', config.debug ? 'text' : 'json')
-		res.send(config.debug ? prettyjson.render(lessons.filter(x => x.lessons == req.params.day)) : JSON.stringify(lessons.filter(x => x.lessons == req.params.day)))
+		res.send(config.debug ? prettyjson.render(lessons.filter(x => x.lessons == req.params.day)) : lessons.filter(x => x.lessons == req.params.day))
         next()
     })
 
@@ -67,7 +67,7 @@ function v1Endpoints(server, rooms, lessons, degrees, courses) {
         const moment = moment(parseInt(req.params.hour).toString() + ":" + parseInt(req.params.minute).toString())
         //res.json(queries.getLessonsDuringMoment(lessons, req.params.day, moment))
 		res.header('content-type', config.debug ? 'text' : 'json')
-		res.send(config.debug ? prettyjson.render(queries.getLessonsDuringMoment(lessons, req.params.day, moment)) : JSON.stringify(queries.getLessonsDuringMoment(lessons, req.params.day, moment)))
+		res.send(config.debug ? prettyjson.render(queries.getLessonsDuringMoment(lessons, req.params.day, moment)) : queries.getLessonsDuringMoment(lessons, req.params.day, moment))
         next()
     })
 
@@ -78,14 +78,14 @@ function v1Endpoints(server, rooms, lessons, degrees, courses) {
         }
         //res.json(lessons.filter(x => isInBuilding(x.room, req.params.bld)))
 		res.header('content-type', config.debug ? 'text' : 'json')
-		res.send(config.debug ? prettyjson.render(lessons.filter(x => isInBuilding(x.room, req.params.bld))) : JSON.stringify(lessons.filter(x => isInBuilding(x.room, req.params.bld))))
+		res.send(config.debug ? prettyjson.render(lessons.filter(x => isInBuilding(x.room, req.params.bld))) : lessons.filter(x => isInBuilding(x.room, req.params.bld)))
         next()
     })
 
     server.get('/v1/lessons/rooms/:room', (req, res, next) => {
         //res.json(lessons.filter(x => x.room == req.params.room))
 		res.header('content-type', config.debug ? 'text' : 'json')
-		res.send(config.debug ? prettyjson.render(lessons.filter(x => x.room == req.params.room)) : JSON.stringify(lessons.filter(x => x.room == req.params.room)))
+		res.send(config.debug ? prettyjson.render(lessons.filter(x => x.room == req.params.room)) : lessons.filter(x => x.room == req.params.room))
         next()
     })
 }
